520d06fbfeabc52e72b109553225139710114d47
Adam Saladino <adam.saladino@icloud.com>
2019-09-08 07:39:35 -0400

    Added books to admin

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    url = models.CharField(max_length=256, null=True, blank=True)
+    thoughts = models.TextField()
+    cover = models.ImageField()
+    finished = models.DateTimeField('date finished')
+
+    def __str__(self):
+        return self.title
